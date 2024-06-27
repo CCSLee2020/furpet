0bc7d06d8cee256eebd953b3ff0d834d5ad85145
CCSLee2020 <202011728@gordoncollege.edu.ph>
2024-06-27 21:54:14 +0800

    updates

--- src/pages/adoptme.tsx
+++ src/pages/adoptme.tsx
@@ -149,7 +149,7 @@ const LandingPage: React.FC = () => {
                             <h2 className="details_h2"><b>Neutered:</b> {pet.neutered}</h2>
                             <h2 className="details_h2"><b>Location:</b> {pet.location}</h2>
                             <h2 className="details_h2"><b>Age &#40;months&#41;:</b> {pet.age}</h2>
-                            <Link to={`${userID}/appointment/${pet.id}`}><div className="details_appointment"><p className="details_appointment2">Take a QnA & Make an &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Appointment</p></div></Link>
+                            <Link to={`${userID}/appointment/${pet.id}`}><div className="details_appointment"><p className="details_appointment2">Make an Appointment</p></div></Link>
                         </div>
                         {imageUrl && <img className="details_img" src={imageUrl} />}
                         <h1 className="details_aboutme">About Me</h1>
